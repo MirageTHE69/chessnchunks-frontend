f9b6f656f8de9c1737251ba115cc30fd4a7121ed
ashwin pathak <ashwin94fc@gmail.com>
2024-10-08 13:54:16 +0530

    added profile images in chat communication

---
+++ src/app/(main)/chat-communication/page.tsx
@@ -0,0 +1,10 @@
+import CommunicationCoach from '@/components/Communication/Communication'
+import React from 'react'
+
+function page() {
+  return (
+    <div><CommunicationCoach /></div>
+  )
+}
+
+export default page
